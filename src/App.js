f0f56c5d103b7a3d1cc82df4352de5aad11dd9c0
John Crisostomo <johncrisostomo@protonmail.ch>
2017-05-26 18:37:25 +0800

    adds conditional logic for signed in/signed out users

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner, Card,CardSection } from './components/common';
 import firebaseSettings from '../firebaseSettings.js';
 
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: null,
+    };
+  }
+
   componentWillMount() {
     firebase.initializeApp(firebaseSettings);
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true});
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button>Log Out</Button>
+          </CardSection>
+        );
+        break;
+      case false:
+        return <LoginForm />;
+        break;
+      default:
+        return (
+          <CardSection>
+            <Spinner size={'large'} />
+          </CardSection>
+        );
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText={"Authentication"} />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
